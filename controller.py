97be31d772190a468c6b461aa2300f0e633f4318
Jose Antonio Ramirez Martinez <100593960+ProgramadorJoseito@users.noreply.github.com>
2022-03-15 17:34:55 -0400

    ejercicio 2 patron mvc
    Jose Antonio Ramirez Martinez cedula 18250853

---
+++ controller.py
@@ -0,0 +1,15 @@
+import modelo
+import vista
+
+
+
+class axiomaTerminalController:
+    def __init__(self):
+        self.model = modelo.axiomaModel()
+        #self.view = vista.vistaAxiomasPorTerminal()
+        self.view = vista.vistaAxiomasHTML()
+
+    def run(self):
+        vNroDeAxiomaEscogido = int(self.view.escojeAxioma())
+        vAxioma = self.model.obtenerAxioma(vNroDeAxiomaEscogido)
+        self.view.mostrar(vAxioma)
---
+++ mvc.py
@@ -0,0 +1,15 @@
+import controller
+
+
+
+def main():
+    #vControlador = controller.axiomaTerminalcontroller()
+    #vControlador.run()
+    
+    vControlador = controller.axiomaTerminalController()
+    vControlador.run()
+    
+
+
+if __name__ == '__main__':
+    main()
